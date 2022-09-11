c496a3958d66ff8831173174eb8f44125556ea8a
ranjan-abhijeet <ranjan.viper@outlook.com>
2022-09-11 15:50:25 +0530

    setup server and client

---
+++ network.py
@@ -0,0 +1,28 @@
+import socket
+
+
+class Network(object):
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "10.42.171.32"
+        self.port = 5555
+        self.addr = (self.server, self.port)
+        self.pos = self.connect()
+
+    def getPos(self):
+        return self.pos
+
+    def connect(self):
+
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except Exception as err:
+            print(f"[-] Error while connecting...: {err}")
+
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            return self.client.recv(2048).decode()
+        except socket.error as err:
+            print(f"[-] Error while sending data...: {err}")
