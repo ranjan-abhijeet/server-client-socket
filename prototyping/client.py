8f8bd0cc4519da0f74d7e8063dad2459d39e89d4
ranjan-abhijeet <ranjan.viper@outlook.com>
2022-09-11 16:12:03 +0530

    refactored code to send objects using pickle instead of string over the network

--- prototyping/client.py
+++ prototyping/client.py
@@ -1,53 +1,11 @@
 import pygame
 from network import Network
+from player import Player
 
 width = 500
 height = 500
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
-
-
-class Player(object):
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.rect = (x, y, width, height)
-        self.vel = 1
-
-    def draw(self, window):
-        pygame.draw.rect(window, self.color, self.rect)
-
-    def move(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.x -= self.vel
-        if keys[pygame.K_RIGHT]:
-            self.x += self.vel
-        if keys[pygame.K_UP]:
-            self.y -= self.vel
-        if keys[pygame.K_DOWN]:
-            self.y += self.vel
-
-        self.update()
-
-    def update(self):
-        self.rect = (self.x, self.y, self.width, self.height)
-
-
-def read_pos(str):
-    try:
-        str = str.split(",")
-        return int(str[0]), int(str[1])
-    except Exception as err:
-        print(str)
-        print("[-] Error")
-
-
-def make_pos(tup):
-    return str(tup[0]) + ","+str(tup[1])
 
 
 def redraw_window(window, p1, p2):
@@ -60,16 +18,10 @@ def redraw_window(window, p1, p2):
 def main():
     run = True
     net = Network()
-    start_pos = read_pos(net.getPos())
-    p1 = Player(start_pos[0], start_pos[1], 50, 50, (0, 255, 0))
-    p2 = Player(0, 0, 50, 50, (255, 0, 0))
-
+    p1 = net.getP()
     while run:
 
-        p2_pos = read_pos(net.send(make_pos((p1.x, p1.y))))
-        p2.x = p2_pos[0]
-        p2.y = p2_pos[1]
-        p2.update()
+        p2 = net.send(p1)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
---
+++ prototyping/player.py
@@ -0,0 +1,31 @@
+import pygame
+
+
+class Player(object):
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = (x, y, width, height)
+        self.vel = 1
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.color, self.rect)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.x -= self.vel
+        if keys[pygame.K_RIGHT]:
+            self.x += self.vel
+        if keys[pygame.K_UP]:
+            self.y -= self.vel
+        if keys[pygame.K_DOWN]:
+            self.y += self.vel
+
+        self.update()
+
+    def update(self):
+        self.rect = (self.x, self.y, self.width, self.height)
